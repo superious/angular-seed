025beeba7e25ab06ae6fa9e0ecfa4ed4c4587b6b
Chris Carlson <cwc@yourmechanic.com>
2015-06-13 02:16:39 -0700

    Add nav bar and additional view. Add search bar.

--- app/app.js
+++ app/app.js
@@ -1,10 +1,10 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.view3',
   'myApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -9,6 +9,6 @@ angular.module('myApp.view2', ['ngRoute'])
   });
 }])
 
-.controller('View2Ctrl', [function() {
-
+.controller('View2Ctrl', '$scope', [function($scope) {
+	$scope.item = 'Hello';
 }]);
